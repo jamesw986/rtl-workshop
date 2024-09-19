9a0f86c90f5ada5d0aac2daf5c118baf34cb7218
Joshua Hiles <joshhiles1@live.co.uk>
2024-09-19 17:43:02 +0100

    chore: Use tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './setupTests.ts',
-  },
+	plugins: [react(), tsconfigPaths()],
+	test: {
+		environment: "jsdom",
+		globals: true,
+		setupFiles: "./setupTests.ts",
+	},
 });
